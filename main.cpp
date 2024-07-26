43c45372c5d205f99483ad492c53df6ca2919e1c
Tomer-Yihya <Tomery49384@gmail.com>
2024-07-27 00:03:09 +0300

    fix main and json files

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 
 
 // getting command line arguments for the house file
-/*int main(int argc, char** argv) {
+int main(int argc, char** argv) {
     
     if (argc < 2) {
         std::cerr << "houseFilePath was not found in the argument list" << std::endl;
@@ -29,50 +29,6 @@
         std::cerr << "Error: " << ex.what() << std::endl;
         return 1;
     }
-    return 0;
-}
-*/
-
-
-// main for Debbuging
-void algorithm_test(std::string file_path, std::string folder_path) {
-    
-    MyAlgorithm algo;
-    MySimulator simulator;
-    simulator.readHouseFile(file_path);
-    simulator.setAlgorithm(algo);
-    simulator.run();
-    simulator.writeOutput(folder_path);
-
-}
-
-
-
-int main() {
-        
-    std::string folder_path = "C:\\Users\\Admin\\Desktop\\CPP Projects\\ex2\\inputs\\";
-    std::string file_path = folder_path;
-    file_path += "input.txt";
-    
-    std::string file_path1 = folder_path;
-    file_path1 += "input1.txt";
-    
-    std::string file_path2 = folder_path;
-    file_path2 += "input2.txt";
-
-    std::string file_path3 = folder_path;
-    file_path3 += "input3.txt";
-
-    std::string file_path4 = folder_path;
-    file_path4 += "input4.txt";
-
-    algorithm_test(file_path, folder_path);
-    algorithm_test(file_path1, folder_path);
-    algorithm_test(file_path2, folder_path);
-    algorithm_test(file_path3, folder_path);
-    algorithm_test(file_path4, folder_path);
-
-
     return 0;
 }
 
